4a821928215aff0a39d272529850c36e004d5a49
Juan Zuluaga <carlos.zuluaga@globant.com>
2019-08-23 11:43:43 -0500

    Fix ulr images

--- MyVet.Web/Data/Entities/Pet.cs
+++ MyVet.Web/Data/Entities/Pet.cs
@@ -26,10 +26,9 @@ namespace MyVet.Web.Data.Entities
 
         public string Remarks { get; set; }
 
-        //TODO: replace the correct URL for the image
         public string ImageFullPath => string.IsNullOrEmpty(ImageUrl)
             ? null
-            : $"https://TBD.azurewebsites.net{ImageUrl.Substring(1)}";
+            : $"https://myvet.azurewebsites.net{ImageUrl.Substring(1)}";
 
         [Display(Name = "Born")]
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
