d29167472a177f4c6e1a3205d81151afe11cc053
Juan Carlos Zuluaga Cardona <jzuluaga55@gmail.com>
2019-10-08 21:16:07 -0500

    Take image

--- MyVet.Prism/MyVet.Prism.Android/Resources/Resource.designer.cs
+++ MyVet.Prism/MyVet.Prism.Android/Resources/Resource.designer.cs
@@ -12391,6 +12391,22 @@ namespace MyVet.Prism.Droid
 			{
 			}
 		}
+		
+		public partial class Xml
+		{
+			
+			// aapt resource value: 0x7F100000
+			public const int file_paths = 2131755008;
+			
+			static Xml()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Xml()
+			{
+			}
+		}
 	}
 }
 #pragma warning restore 1591
--- MyVet.Prism/MyVet.Prism/ViewModels/EditPetViewModel.cs
+++ MyVet.Prism/MyVet.Prism/ViewModels/EditPetViewModel.cs
@@ -2,6 +2,9 @@
 using MyVet.Common.Models;
 using MyVet.Common.Services;
 using Newtonsoft.Json;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Prism.Commands;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
@@ -22,6 +25,8 @@ namespace MyVet.Prism.ViewModels
         private bool _isEdit;
         private ObservableCollection<PetTypeResponse> _petTypes;
         private PetTypeResponse _petType;
+        private MediaFile _file;
+        private DelegateCommand _changeImageCommand;
 
         public EditPetViewModel(
             INavigationService navigationService,
@@ -31,6 +36,8 @@ namespace MyVet.Prism.ViewModels
             _navigationService = navigationService;
             _apiService = apiService;
         }
+
+        public DelegateCommand ChangeImageCommand => _changeImageCommand ?? (_changeImageCommand = new DelegateCommand(ChangeImageAsync));
 
         public ObservableCollection<PetTypeResponse> PetTypes
         {
@@ -143,6 +150,49 @@ namespace MyVet.Prism.ViewModels
             if (!string.IsNullOrEmpty(Pet.PetType))
             {
                 PetType = PetTypes.FirstOrDefault(pt => pt.Name == Pet.PetType);
+            }
+        }
+
+        private async void ChangeImageAsync()
+        {
+            await CrossMedia.Current.Initialize();
+
+            var source = await Application.Current.MainPage.DisplayActionSheet(
+                "Where do you want to get the picture from?",
+                "Cancel",
+                null,
+                "From Gallery",
+                "From Camera");
+
+            if (source == "Cancel")
+            {
+                _file = null;
+                return;
+            }
+
+            if (source == "From Camera")
+            {
+                _file = await CrossMedia.Current.TakePhotoAsync(
+                    new StoreCameraMediaOptions
+                    {
+                        Directory = "Sample",
+                        Name = "test.jpg",
+                        PhotoSize = PhotoSize.Small,
+                    }
+                );
+            }
+            else
+            {
+                _file = await CrossMedia.Current.PickPhotoAsync();
+            }
+
+            if (_file != null)
+            {
+                ImageSource = ImageSource.FromStream(() =>
+                {
+                    var stream = _file.GetStream();
+                    return stream;
+                });
             }
         }
     }
