bdbfcef0f487cdfb795086233124c938f6b6d5ae
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2019-10-29 20:05:44 -0500

    Multi-language

---
+++ MyVet.Prism/MyVet.Prism/Helpers/Languages.cs
@@ -0,0 +1,42 @@
+﻿using MyVet.Prism.Interfaces;
+using MyVet.Prism.Resources;
+using Xamarin.Forms;
+
+namespace MyVet.Prism.Helpers
+{
+    public static class Languages
+    {
+        static Languages()
+        {
+            var ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            Resource.Culture = ci;
+            DependencyService.Get<ILocalize>().SetLocale(ci);
+        }
+
+        public static string Accept => Resource.Accept;
+
+        public static string Error => Resource.Error;
+
+        public static string EmailError => Resource.EmailError;
+
+        public static string Email => Resource.Email;
+
+        public static string EmailPlaceHolder => Resource.EmailPlaceHolder;
+
+        public static string ForgotPassword => Resource.ForgotPassword;
+
+        public static string Login => Resource.Login;
+
+        public static string LoginError => Resource.LoginError;
+
+        public static string Password => Resource.Password;
+
+        public static string PasswordError => Resource.PasswordError;
+
+        public static string PasswordPlaceHolder => Resource.PasswordPlaceHolder;
+
+        public static string Register => Resource.Register;
+
+        public static string Rememberme => Resource.Rememberme;
+    }
+}
