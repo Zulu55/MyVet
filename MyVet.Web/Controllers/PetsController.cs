83cba0d85cabf9c8e575a9943037fc45edce10b8
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2019-11-08 11:16:47 -0500

    Improve PDF generation

--- MyVet.Web/Controllers/PetsController.cs
+++ MyVet.Web/Controllers/PetsController.cs
@@ -28,11 +28,21 @@ namespace MyVet.Web.Controllers
 
         public IActionResult Index()
         {
-            return new ViewAsPdf(_dataContext.Pets
+            return View(_dataContext.Pets
                 .Include(p => p.Owner)
                 .ThenInclude(o => o.User)
                 .Include(p => p.PetType)
                 .Include(p => p.Histories));
+        }
+
+        public IActionResult Print()
+        {
+            return new ViewAsPdf(_dataContext.Pets
+                .Include(p => p.Owner)
+                .ThenInclude(o => o.User)
+                .Include(p => p.PetType)
+                .Include(p => p.Histories))
+                { FileName = "Pets.pdf" };
         }
 
         public async Task<IActionResult> Details(int? id)
